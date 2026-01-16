46ae18c716e00ff38098317d8960f2eba90503d1
Whitemehat <wong1234.pe@gmail.com>
2026-01-16 19:37:24 +0700

    7

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,1 +1,30 @@
-export function safeFetchUser() {}
+import axios from "axios"
+const API = "https://jsonplaceholder.typicode.com/users";
+
+type User = {
+    id: number,
+    name: string,
+    address: {},
+    phone: string,
+}
+
+export async function safeFetchUser(id: number) {
+    try{
+        const res = await axios.get(API);
+        const data = res.data;
+        let result: User;
+        for(const user of data){
+        if(user.id === id){
+            return result = {
+                id: user.id,
+                name: user.name,
+                address: user.address,
+                phone: user.phone
+            };
+        }
+    }
+    }
+    catch(err){
+        return null;
+    }
+}
